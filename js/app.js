62ce95e1e6e7a239bb3e1a3f3a723af94d0b8cfc
SamanthaH1 <wisconsinflower2003@yahoo.com>
2016-02-16 15:43:38 -0600

    Final Changes

--- js/app.js
+++ js/app.js
@@ -1,26 +1,53 @@
 $(document).ready(function(){
-	$('.ryu').mouseenter(function() {
-//		alert('mouse enter .ryu div');
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-//		alert('mouse leave');
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function() {
-		console.log('mousedown');
-		// play hadouken sound
-		  $('.ryu-ready').hide();
-  		  $('.ryu-throwing').show();
- 		  $('.hadouken').show();
-		//show hadouken and animate it to the right of the screen
-	})
-	.mouseup(function() {
-		console.log('mouseup');
-		//ryu goes back to his ready position
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	});
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+
+$("body").keydown(function(samanthasevent){
+     console.log(arguments)
+      if (samanthasevent.which == 88) {
+        $('.ryu-cool').show();
+        $('.ryu-ready').hide();
+        $('.ryu-still').hide();
+        console.log("this must be the x key");
+      }
+    });
+
+$("body").keyup(function(samanthasevent){
+      if (samanthasevent.which == 88) {
+        $('.ryu-cool').hide();
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+
+      }
+    });
+
+
 });
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
